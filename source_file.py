e433d15ba3222fdeda1c2ee6ecdc664629bafaf3
trarck <trarck@gmail.com>
2018-09-06 10:53:00 +0800

    加入对代码文件的操作。

---
+++ source_file.py
@@ -0,0 +1,84 @@
+class SourceFile:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.content = None
+
+    def open(self):
+        fp = open(self.file_path, 'r')
+        self.content = fp.read()
+        fp.close()
+
+    def save(self):
+        fp = open(self.file_path, 'w')
+        fp.write(self.content)
+        fp.close()
+
+    def insert(self, keys, words):
+        pos = 0
+        for k in keys:
+            pos = self.content.find(k, pos)
+            if pos == -1:
+                return False
+            pos += len(k)
+        self.content = self.content[:pos] + words + self.content[pos:]
+
+    def insert_before(self, keys, words):
+        pos = 0
+        next_pos = 0
+        for k in keys:
+            pos = self.content.find(k, next_pos)
+            if pos == -1:
+                return False
+            next_pos = pos + len(k)
+        self.content = self.content[:pos] + words + self.content[pos:]
+
+    def replace(self, froms, tos, words):
+        from_pos = 0
+        for k in froms:
+            from_pos = self.content.find(k, from_pos)
+            if from_pos == -1:
+                return False
+            from_pos += len(k)
+
+        to_pos = 0
+        find_next_pos = from_pos
+        for k in tos:
+            to_pos = self.content.find(k, find_next_pos)
+            if to_pos == -1:
+                return False
+            find_next_pos = to_pos + len(k)
+        self.content = self.content[:from_pos] + words + self.content[to_pos:]
+
+    def replace_after(self, froms, tos, words):
+        from_pos = 0
+        for k in froms:
+            from_pos = self.content.find(k, from_pos)
+            if from_pos == -1:
+                return False
+            from_pos += len(k)
+
+        to_pos = from_pos
+
+        for k in tos:
+            to_pos = self.content.find(k, to_pos)
+            if to_pos == -1:
+                return False
+            to_pos += len(k)
+        self.content = self.content[:from_pos] + words + self.content[to_pos:]
+
+    def remove(self, froms, tos):
+        from_pos = 0
+        for k in froms:
+            from_pos = self.content.find(k, from_pos)
+            if from_pos == -1:
+                return False
+            from_pos += len(k)
+
+        to_pos = 0
+        find_next_pos = from_pos
+        for k in tos:
+            to_pos = self.content.find(k, find_next_pos)
+            if to_pos == -1:
+                return False
+            find_next_pos = to_pos + len(k)
+        self.content = self.content[:from_pos] + self.content[to_pos:]
--- testPbxProject.py
+++ testPbxProject.py
@@ -1,12 +1,6 @@
-from pbxproj import XcodeProject
-# open the project
-project = XcodeProject.load('data/Unity-iPhone.xcodeproj/project.pbxproj')
-
-# add a file to it, force=false to not add it if it's already in the project
-#project.add_file('MyClass.swift', force=False)
-
-# set a Other Linker Flags
-project.add_other_ldflags('-ObjC')
-
-# save the project, otherwise your changes won't be picked up by Xcode
-project.save()
+import os
+from source_file import SourceFile
+sf=SourceFile("data/temp/t.txt")
+sf.open()
+sf.replace_after(["dd","bb"],["bb"],"cccc")
+sf.save()
