ee05984b0ea2f208f4432eab8fbb5eea9fd71c04
trarck <trarck@gmail.com>
2018-09-20 15:21:41 +0800

    temp

---
+++ actions/copy_files.py
@@ -0,0 +1,3 @@
+class CopyFilesAction:
+    def run(self,config,src_path,dst_path):
+        print("===>copy files from %s to %s"%(src_path,dst_path))
---
+++ use.md
@@ -0,0 +1,5 @@
+一、安装python依赖库
+    pip install pbxproj
+	pip install Cheetah
+二、配置工程
+    1.新建一个工作目录
