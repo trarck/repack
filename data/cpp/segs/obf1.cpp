3cf2facd6f21c6671387760324fbb1a8677cec88
nslm <duanhh@duanhouhaideMac-Pro.local>
2019-01-02 12:31:15 +0800

    fix mapping to segs

--- data/cpp/segs/obf1.cpp
+++ data/cpp/segs/obf1.cpp
@@ -1,10 +1,29 @@
 
 /**	add by obf tools start */
+
 static std::string s_obfuscateKey = "abcd";
 
 void FileUtils::setObfuscateKey(const std::string & key)
 {
 	s_obfuscateKey = key;
+}
+
+void FileUtils::setObfNameDictionary(const ValueMap& nameDict)
+{
+	_obfNameDict = nameDict;
+}
+
+void  FileUtils::loadObfNameDictionaryFromFile(const std::string &filename)
+{
+	const std::string fullPath = fullPathForFilename(filename);
+	if (!fullPath.empty())
+	{
+		ValueMap dict = FileUtils::getInstance()->getValueMapFromFile(fullPath);
+		if (!dict.empty())
+		{
+			setObfNameDictionary(dict);
+		}
+	}
 }
 
 void FileUtils::addObfuscateParentPath(const std::string &parentPath, const bool front)
@@ -39,60 +58,68 @@ void FileUtils::addObfuscateSubPath(const std::string &subPath, const bool front
 	}
 }
 
-std::string FileUtils::getPathHash(const std::string& filepath) const
+std::string FileUtils::getObfTargetPath(const std::string& filepath) const
 {
-	
-	std::string obfFilepath =s_obfuscateKey + "_" + filepath;
-	Data data;
-	data.copy((const unsigned char*)(obfFilepath.c_str()), obfFilepath.length());
-	std::string crypt= utils::getDataMD5Hash(data);
-	return crypt.substr(0, 1) + "/" + crypt;
+	if (!s_obfuscateKey.empty()) {
+		std::string obfFilepath = s_obfuscateKey + "_" + filepath;
+		Data data;
+		data.copy((const unsigned char*)(obfFilepath.c_str()), obfFilepath.length());
+		std::string crypt = utils::getDataMD5Hash(data);
+		auto iter = _obfNameDict.find(crypt);
+
+		if (iter == _obfNameDict.end())
+		{
+			return crypt.substr(0, 1) + "/" + crypt;
+		}
+		else
+		{
+			return iter->second.asString();
+		}
+	}
+	else
+	{
+		auto iter = _obfNameDict.find(filepath);
+
+		if (iter == _obfNameDict.end())
+		{
+			return filepath;
+		}
+		else
+		{
+			return iter->second.asString();
+		}
+	}
 }
 
 
 std::string FileUtils::getObfuscatePathAndCheckSub(const std::string& filepath, const std::string& searchPath) const
 {
-    std::string path;
-    std::string obf_file_path;
-    
-    std::string file_ext=getFileExtension(filepath);
-    //check sub path
-    //sub path like [hd,cocosress]
-    //file_path a/b.png
-    //will ceck hd/a/b.png,cocosress/a/b.png
-    for (const auto& subIt : _obfuscateSubPaths)
-    {
-        obf_file_path = subIt + filepath;
-        obf_file_path = getPathHash(obf_file_path);
-        path = getFullPathForDirectoryAndFilename(searchPath, obf_file_path);
-        if (!path.empty()) {
-            return path;
-        }
-        
-        //check with ext
-        obf_file_path+=file_ext;
-        path = getFullPathForDirectoryAndFilename(searchPath, obf_file_path);
-        if (!path.empty()) {
-            return path;
-        }
-    }
-    
-    //check orignal path
-    obf_file_path = filepath;
-    obf_file_path = getPathHash(obf_file_path);
-    path = getFullPathForDirectoryAndFilename(searchPath, obf_file_path);
-    if (!path.empty()) {
-        return path;
-    }
-    
-    //check with ext
-    obf_file_path+=file_ext;
-    path = getFullPathForDirectoryAndFilename(searchPath, obf_file_path);
-    if (!path.empty()) {
-        return path;
-    }
-    
-    return "";
+	std::string path;
+	std::string obf_file_path;
+
+	//check sub path
+	//sub path like [hd,cocosress]
+	//file_path a/b.png
+	//will ceck hd/a/b.png,cocosress/a/b.png
+	for (const auto& subIt : _obfuscateSubPaths)
+	{
+		obf_file_path = subIt + filepath;
+		obf_file_path = getObfTargetPath(obf_file_path);
+		path = getFullPathForDirectoryAndFilename(searchPath, obf_file_path);
+		if (!path.empty()) {
+			return path;
+		}
+	}
+
+	//check orignal path
+	obf_file_path = filepath;
+	obf_file_path = getObfTargetPath(obf_file_path);
+	path = getFullPathForDirectoryAndFilename(searchPath, obf_file_path);
+	if (!path.empty()) {
+		return path;
+	}
+
+	return "";
 }
 
 //remove parent path
@@ -103,35 +130,21 @@ std::string FileUtils::getObfuscatePathAndCheckSub(const std::string& filepath, 
 //file_path res/a/b.png will check a/b.png
 std::string FileUtils::getObfuscatePathCheckParent(const std::string& filepath, const std::string& searchPath) const
 {
-    if(_obfuscateParentPaths.size()==0){
-        return "";
-    }
-    
-    std::string path;
-    std::string obf_file_path= filepath;
-    std::string file_ext=getFileExtension(filepath);
-    
-    for (const auto& parentIt : _obfuscateParentPaths)
-    {
-        int pos = obf_file_path.find(parentIt);
-        if (pos == 0) {
-            obf_file_path = obf_file_path.substr(parentIt.length());
-            path = getObfuscatePathAndCheckSub(obf_file_path, searchPath);
-            if (!path.empty()) {
-                return path;
-            }
-            
-            //check with ext
-            obf_file_path+=file_ext;
-            path = getFullPathForDirectoryAndFilename(searchPath, obf_file_path);
-            if (!path.empty()) {
-                return path;
-            }
-            
-            obf_file_path = filepath;
-        }
-    }
-    return "";
+	std::string path;
+	std::string obf_file_path= filepath;
+	for (const auto& parentIt : _obfuscateParentPaths)
+	{
+		int pos = obf_file_path.find(parentIt);
+		if (pos == 0) {
+			obf_file_path = obf_file_path.substr(parentIt.length());
+			path = getObfuscatePathAndCheckSub(obf_file_path, searchPath);
+			if (!path.empty()) {
+				return path;
+			}
+			obf_file_path = filepath;
+		}
+	}
+	return "";
 }
 
 std::string FileUtils::getObfuscatePath(const std::string& filepath,const std::string& searchPath) const
--- data/cpp/segs/obf_head.h
+++ data/cpp/segs/obf_head.h
@@ -2,11 +2,14 @@
 /**add by obf tools start*/
 	std::vector<std::string> _obfuscateParentPaths;
 	std::vector<std::string> _obfuscateSubPaths;
+	ValueMap _obfNameDict;
 public:
 	static void setObfuscateKey(const std::string & key);
 	void addObfuscateParentPath(const std::string & path, const bool front = false);
 	void addObfuscateSubPath(const std::string & path, const bool front = false);
-	std::string getPathHash(const std::string& filepath) const;
+	void setObfNameDictionary(const ValueMap& nameDict);
+	void loadObfNameDictionaryFromFile(const std::string &filename);
+	std::string getObfTargetPath(const std::string& filepath) const;
 	std::string getObfuscatePathAndCheckSub(const std::string& filepath, const std::string& searchPath) const;
 	std::string getObfuscatePathCheckParent(const std::string& filepath, const std::string& searchPath) const;
 	std::string getObfuscatePath(const std::string& filepath,const std::string& searchPath) const;
