f66c7d2f020b5209996849a40c6304350b1474d2
nslm <duanhh@duanhouhaideMac-Pro.local>
2019-01-04 19:27:41 +0800

    test

--- tests/test_cpp_parse.py
+++ tests/test_cpp_parse.py
@@ -4,14 +4,14 @@ from pprint import pprint
 parser = Parser({
     "clang_args": [
         "-x", "c++",
-        "-arch", "arm64", "-std=c++11", "-stdlib=libc++",
-        "-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1",
+        "-std=c++11", "-stdlib=libc++",
+        "-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS12.1.sdk",
         "-I/usr/local/include",
+        "-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1",
         "-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/10.0.0/include",
         "-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include",
         "-I/usr/include",
-        "-I/System/Library/Frameworks",
-        "-I/Library/Frameworks",
+        "-F/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks",
         "-I/Users/duanhh/workspace/load_pack/tempprojects/test3/frameworks/cocos2d-x/cocos",
         "-I/Users/duanhh/workspace/load_pack/tempprojects/test3/frameworks/cocos2d-x/external",
         "-I/Users/duanhh/workspace/load_pack/tempprojects/test3/frameworks/cocos2d-x/extensions",
@@ -24,12 +24,10 @@ parser = Parser({
         "-I/Users/duanhh/workspace/load_pack/tempprojects/test3/frameworks/cocos2d-x/external/websockets/include/ios",
         "-I/Users/duanhh/workspace/load_pack/tempprojects/test3/frameworks/cocos2d-x/external/freetype2/include/ios/freetype2",
         "-I/Users/duanhh/workspace/load_pack/tempprojects/test3/frameworks/cocos2d-x/external/external/fmod/include",
-        "-DCC_TARGET_OS_IPHONE", "-DCC_ENABLE_CHIPMUNK_INTEGRATION=1", "-DNDEBUG", "-DUSE_FILE32API",
-        "-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS12.1.sdk"
-
+        "-DCC_TARGET_OS_IPHONE", "-DCC_ENABLE_CHIPMUNK_INTEGRATION=1", "-DNDEBUG", "-DUSE_FILE32API"
     ],
     "tpl_folder": "../data/template/obf"
 })
 
-ast = parser.get_ast("../data/temp/inject/CCSprite3D.cpp");
-# pprint(('nodes',ast))
+ast = parser.get_ast("../data/temp/inject/CCSprite3d.cpp");
+pprint(('nodes',ast))
