e9be59c0c4962a21e4f86a9869e539414b12a276
trarck <trarck@gmail.com>
2019-01-15 20:49:18 +0800

    格式整理。

--- tests/test_cpp_gen.py
+++ tests/test_cpp_gen.py
@@ -3,11 +3,12 @@ import unittest
 from garbage_code.cpp_generator import *
 from garbage_code.cpp_garbage_code import CppFile
 
+
 class CppGenTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print("in setup class")
-        cls.tpl_folder_path= os.path.join(os.path.dirname(__file__), "../data/template/cpp")
+        cls.tpl_folder_path = os.path.join(os.path.dirname(__file__), "../data/template/cpp")
 
     @classmethod
     def tearDownClass(cls):
@@ -25,19 +26,19 @@ class CppGenTest(unittest.TestCase):
         parameter_count = 3
         for i in range(parameter_count):
             param_type = CType("int")
-            parameter = CParameter("param%d"%i, param_type)
+            parameter = CParameter("param%d" % i, param_type)
             parameters.append(parameter)
 
         return_type = CType("float")
 
         fun = CppMethod(method_name, parameters, return_type, self.tpl_folder_path)
 
-        self.assertEqual(fun.get_def_string().strip(),"virtual float myfun(int param0,int param1,int param2);")
+        self.assertEqual(fun.get_def_string().strip(), "virtual float myfun(int param0,int param1,int param2);")
         self.assertIsNotNone(fun.get_call_string())
         self.assertIsNotNone(fun.get_code_string())
 
     def test_gen_class(self):
-        cls= CppGenerator.generate_class(self.tpl_folder_path,3,5,3,80)
+        cls = CppGenerator.generate_class(self.tpl_folder_path, 3, 5, 3, 80)
         self.assertIsNotNone(cls.get_def_string())
         self.assertIsNotNone(cls.get_code_string())
 
@@ -55,7 +56,7 @@ class CppGenTest(unittest.TestCase):
             "parameter_count": 3,
             "return_probability": 80,
             "call_others": True,
-            "search_path":"cpp_files/gen"
+            "search_path": "cpp_files/gen"
         })
 
         generator.prepare()
@@ -63,6 +64,7 @@ class CppGenTest(unittest.TestCase):
 
         self.assertTrue(os.path.exists(head_file_name))
         self.assertTrue(os.path.exists(source_file_name))
+
 
 if __name__ == '__main__':
     unittest.main()
