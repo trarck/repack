8ef234cdc0bc12fbdf87825fe67d311fb997a303
trarck <trarck@gmail.com>
2019-01-16 14:10:31 +0800

    加入带命名空间的类。

---
+++ tests/cpp_files/c.cpp
@@ -0,0 +1,65 @@
+#include "a.h"
+
+namespace foo
+{
+C::C()
+{
+    
+}
+
+C::~C()
+{}
+
+void C::ma(){
+    int i=0;
+    i+=3;
+}
+
+int C::ia()
+{
+int i=0;
+int j=0;
+    i+=3;
+j*=4;
+    return i;
+}
+
+float C::fa(){
+    float i=0;
+    i+=3;
+    return i;
+}
+
+}
+
+namespace bar
+{
+D::D()
+{
+    
+}
+
+D::~D()
+{}
+
+void D::ma(){
+int i=0;
+i+=3;
+}
+
+int D::ia()
+{
+int i=0;
+int j=0;
+    i+=3;
+j*=4;
+    return i;
+}
+
+float D::fa(){
+    float i=0;
+    i+=3;
+    return i;
+}
+
+}
---
+++ tests/cpp_files/c.h
@@ -0,0 +1,21 @@
+namespace foo{
+    class C{
+    public:
+        C();
+        ~C();
+        void ma();
+        int ia();
+        float fa();
+    };
+}
+
+namespace bar{
+    class D{
+    public:
+        D();
+        ~D();
+        void ma();
+        int ia();
+        float fa();
+    };
+}
